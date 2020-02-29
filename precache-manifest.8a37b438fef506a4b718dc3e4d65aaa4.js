b170b51155ed69d90c4656bc41e24faf98aed5b9
Rythae <auxano42@gmail.com>
2020-02-29 22:05:25 +0100

    Updates

---
+++ precache-manifest.8a37b438fef506a4b718dc3e4d65aaa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038c77c1fe1531031d35eebd8ef961d2",
+    "url": "/RITA-EMILI-Shecluded-Frontend-Task-2020/index.html"
+  },
+  {
+    "revision": "08eb0eac7f4915fe5c45",
+    "url": "/RITA-EMILI-Shecluded-Frontend-Task-2020/static/css/main.e1a8786a.chunk.css"
+  },
+  {
+    "revision": "5ecbd21d8b196ad1cf68",
+    "url": "/RITA-EMILI-Shecluded-Frontend-Task-2020/static/js/2.06b3cdc7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/RITA-EMILI-Shecluded-Frontend-Task-2020/static/js/2.06b3cdc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08eb0eac7f4915fe5c45",
+    "url": "/RITA-EMILI-Shecluded-Frontend-Task-2020/static/js/main.eb297338.chunk.js"
+  },
+  {
+    "revision": "b0a669f3dc804af359fa",
+    "url": "/RITA-EMILI-Shecluded-Frontend-Task-2020/static/js/runtime-main.8ae1a6ee.js"
+  }
+]);
